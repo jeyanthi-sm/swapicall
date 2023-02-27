1af62c0b40effd91b610da697269f0400396e268
Meenashi <jeyanthi_sm@yahoo.com>
2023-02-27 05:42:50 +0000

    Updated the test cases to test the mock API call. Works Fine.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { setupServer } from 'msw/node'
+import { rest, setupWorker } from 'msw';
 
-test('renders learn react link', () => {
+// This configures a request mocking server with the given request handlers.
+const server = setupServer()
+
+test('renders headers swapi simply call', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/swapi simply call/i);
+  expect(headerElement).toBeInTheDocument();
 });
+
+const json1 = {
+  count: 82,
+  next: "https://swapi.dev/api/people/?page=2",
+  previous: null,
+  results: [
+    {
+      name: "Mock Character1",
+    },
+  ],
+};
+
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test("loads and displays the first Mock character", async () => {
+  render(<App />);
+
+  
+  server.use(
+    rest.get(`https://swapi.dev/api/people/`, (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(json1));
+    })
+  );
+
+  
+  await screen.findByText(json1.results[0].name);
+  expect(screen.getByText(json1.results[0].name)).toBeInTheDocument();
+});
+

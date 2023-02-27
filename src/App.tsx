deff43bb33be255eb332fe185f7d25abeb814f06
Meenashi <jeyanthi_sm@yahoo.com>
2023-02-27 05:14:02 +0000

    Fetched SWAPI characters and displayed the first character

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [swapiGetcharacters, setSwapiGetCharacters] = useState("");
+  const SWAPIURL = `https://swapi.dev/api/people/`;
+    useEffect(() => {
+    const fetchSwapiCharacters = async () => {
+      try {
+        const response = await fetch(`https://swapi.dev/api/people/?page=1`);
+        if (response.ok) {
+        const json = await response.json();
+        console.log(json.results[0].name);
+        setSwapiGetCharacters(json.results[0].name);
+        
+        }
+      } catch (error) {
+        console.log("error");
+      }
+    };
+    fetchSwapiCharacters();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <h1>   SWAPI Simply Call  </h1>
       </header>
+      <body> 
+         <div> {swapiGetcharacters} </div>
+      </body>
     </div>
   );
 }

17842d702e69d4019c5ed4b2d751657f33d3172d
Meenashi <jeyanthi_sm@yahoo.com>
2023-03-03 07:19:07 +0000

    Handled 418 and 500 errors.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { setupServer } from 'msw/node'
-import { rest, setupWorker } from 'msw';
+import { rest } from 'msw';
 
 // This configures a request mocking server with the given request handlers.
 const server = setupServer()
@@ -14,9 +14,6 @@ test('renders headers swapi simply call', () => {
 });
 
 const json1 = {
-  count: 82,
-  next: "https://swapi.dev/api/people/?page=2",
-  previous: null,
   results: [
     {
       name: "Mock Character1",
@@ -38,9 +35,55 @@ test("loads and displays the first Mock character", async () => {
       return res(ctx.status(200), ctx.json(json1));
     })
   );
-
-  
   await screen.findByText(json1.results[0].name);
   expect(screen.getByText(json1.results[0].name)).toBeInTheDocument();
 });
+
+const json500 = {
+      "detail": "Oops... something went wrong, try again 🤕",
+};
+
+ 
+
+test("loads 500 Error", async () => {
+  render(<App />);
+
+  server.use(
+    rest.get(`https://swapi.dev/api/people/`, (req, res, ctx) => {
+    return res(ctx.status(500), 
+    ctx.set('Content-Type', 'application/json'), ctx.json(json500));
+    })
+  );
+    
+ 
+  
+  await screen.findByText(json500.detail);
+  expect(screen.getByText(json500.detail)).toBeInTheDocument();
+});
+
+const json418 =  {
+      "detail": "418 I'm a tea pot, silly",
+    }
+
+ 
+
+test("loads 418 Error", async () => {
+  render(<App />);
+
+  
+  server.use(
+    rest.get(`https://swapi.dev/api/people/`, (req, res, ctx) => {
+      console.log(ctx.status);
+      console.log(ctx.json);  
+        return res(ctx.status(418), ctx.json(json418));
+    })
+  );
+
+  
+  await screen.findByText(json418.detail);
+  expect(screen.getByText(json418.detail)).toBeInTheDocument();
+});
+
+
+ 
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [swapiGetcharacters, setSwapiGetCharacters] = useState("");
-  const SWAPIURL = `https://swapi.dev/api/people/`;
+  const [swapiGetcharacters, setSwapiGetCharacters] = useState();
+  const [errorMessage, setErrorMessage] = useState<string>();
+  //const [swapiError, setSwapiError] = useState<string>();
+  const SWAPIURL = `https://swapi.dev/api/people/?page=1`;
     useEffect(() => {
     const fetchSwapiCharacters = async () => {
       try {
-        const response = await fetch(`https://swapi.dev/api/people/?page=1`);
+        const response = await fetch(SWAPIURL);
         if (response.ok) {
         const json = await response.json();
         console.log(json.results[0].name);
-        setSwapiGetCharacters(json.results[0].name);
-        
+        setSwapiGetCharacters(json.results[0].name);        
         }
-      } catch (error) {
+        else if ((response.status === 418) || (response.status === 500))
+        {
+          const json = await response.json();
+          setErrorMessage(json.detail);  
+        }
+      }
+       catch (error) {
+        if (typeof error === "string")
+        {
         console.log("error");
+        setErrorMessage("some message");
+        }
       }
     };
     fetchSwapiCharacters();
@@ -27,7 +37,7 @@ function App() {
         <h1>   SWAPI Simply Call  </h1>
       </header>
       <body> 
-         <div> {swapiGetcharacters} </div>
+         <div> {swapiGetcharacters} {errorMessage} </div>
       </body>
     </div>
   );
